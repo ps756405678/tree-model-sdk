3c0791564e3dd400eff231fb584ccae5cab9161b
ps120 <ps120_studio@outlook.com>
2023-08-23 10:43:54 +0800

    update

--- domain/domain.go
+++ domain/domain.go
@@ -6,6 +6,13 @@ type TreeNode struct {
 	ID         string    `json:"_id"`
 	Title      string    `json:"title"`
 	ParentId   string    `json:"parent_id"`
+	Data       any       `json:"data"`
 	CreateTime time.Time `json:"create_time"`
 	UpdateTime time.Time `json:"update_time"`
+}
+
+type Result struct {
+	ErrCode    int    `json:"err_code"`
+	ErrMessage string `json:"err_message"`
+	Data       any    `json:"data"`
 }
